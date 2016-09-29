748bff0393691c127517c52846c0c08291548c02
Yuuta Sumioka <ysumio120@yahoo.com>
2016-09-29 03:48:29 -0400

    Stattleship API - change navbar color based on team searched

--- assets/javascript/facebookapi.js
+++ assets/javascript/facebookapi.js
@@ -1,13 +1,104 @@
+// Facebook App Token
 var token = "627047640810455|45zQTmaJMlTO45dEj2hOqNUtAug";
+
+var teamArr = [];
+
+stattleshipSearch();
 
 $("#submitBtn").on("click", function() {
 	var team = $(".form-control").val();
 	var team_id;
 
-	ajaxCall(team);
+	ajaxFacebook(team);
+	teamColor(team);
+
+	return false;
 });
 
-function ajaxCall(team) {
+function teamObj(color, name, id) {
+	var team = {
+		color: color,
+		name: name,
+		id: id
+	};
+
+	return team;
+}
+
+function stattleshipSearch() {
+
+	var urlArr = [];
+	var nflTeams = "https://api.stattleship.com/football/nfl/teams";
+	var mlbTeams = "https://api.stattleship.com/baseball/mlb/teams";
+	var nbaTeams = "https://api.stattleship.com/basketball/nba/teams";
+	var nhlTeams = "https://api.stattleship.com/hockey/nhl/teams";
+
+	urlArr.push(nflTeams);
+	urlArr.push(mlbTeams);
+	urlArr.push(nbaTeams);
+	urlArr.push(nhlTeams);
+
+	ajaxStattleship(urlArr);
+
+	console.log(teamArr);
+
+};
+
+function teamColor(team) {
+	for(var i = 0; i < teamArr.length; i++) {
+		var teamObj = teamArr[i];
+		if(team.toLowerCase() == teamObj.name.toLowerCase()) {
+			console.log(teamObj.color);
+			$('.navbar').css("background-color", "#" + teamObj.color);
+			return;
+		}
+	}
+}
+
+
+function ajaxStattleship(urlArr) {
+	for(var i = 0; i < urlArr.length; i++) {
+		$.ajax({
+			url: urlArr[i],
+			method: "GET",
+			headers: {
+		      "Authorization": "Token token=b1ab3c93495c159d440885c2a6a92430",
+		      "Accept": "application/vnd.stattleship.com; version=1",
+		      'Content-Type': 'application/json',
+		    },
+		    async: false,
+			// data: {'Authorization': 'Token token=b1ab3c93495c159d440885c2a6a92430',
+			// 		'Content-Type': 'application/json',
+			// 		'Accept': 'application/vnd.stattleship.com; version=1'},
+			success: function(data) {
+				console.log(data);
+				for(var i = 0; i < data.teams.length; i++) {
+					var team = data.teams[i];
+					var location = team.location;
+					var name = team.nickname;
+					var full_name = location + " " + name;
+					var color = team.color;
+					var team_id = team.slug;
+
+					console.log(team_id);
+
+					teamArr.push(teamObj(color, full_name, team_id));
+
+				}
+				
+			},
+			error: function(data){
+				console.log(data); // send the error notifications to console
+			}
+
+		});
+	}
+
+	//console.log(teamArr);
+}
+
+
+function ajaxFacebook(team) {
 	var team_id;
 
 	$.ajax({
@@ -16,6 +107,7 @@ function ajaxCall(team) {
 		dataType: 'jsonp',
 		type: 'GET',
 		data: {q: team, type: 'page', 'fields': "name, category, posts", access_token: token},
+		async: false,
 		success: function(data){
 			console.log(data);
 			for(i = 0; i < data.data.length; i++) {
